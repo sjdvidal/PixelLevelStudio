6811862adf53a91ab9d9c9a03e689a74aea8513a
Diego Vidal <sjdvidal@micoope.coosajo.com>
2025-04-18 16:46:47 -0600

    nuevas modificaicones

---
+++ main.js
@@ -0,0 +1,4 @@
+const bootstrapScript = document.createElement('script');
+bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+bootstrapScript.defer = true;
+document.body.appendChild(bootstrapScript);
